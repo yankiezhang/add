92ff00b71e4d2bad6b61d65c327ae1f26cfeb972
yang <yankiezhang@gmail.com>
2023-03-28 22:43:53 +0800

    Initial Commit

---
+++ add/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  add
+//
+//  Created by Yang Zhang on 2023/3/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
